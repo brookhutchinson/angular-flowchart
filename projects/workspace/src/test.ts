070ed71498811224d38a78ea2d1f443587d7980f
Brook Hutchinson <Brookhutchinson@outlook.com>
2022-12-22 07:49:08 -0600

    Improve abbility to maintain code

--- projects/workspace/src/test.ts
+++ projects/workspace/src/test.ts
@@ -1,16 +1,12 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
+// this file is required by karma.conf.js and loads recursively all the .spec and framework files
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-// First, initialize the Angular testing environment.
+// initialize the Angular testing environment
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(), {
     teardown: { destroyAfterEach: false }
-}
+  }
 );
