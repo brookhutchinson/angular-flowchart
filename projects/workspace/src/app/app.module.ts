ee649882b9ed2a8a5f7736c7429e544804e736fc
Brook Hutchinson <Brookhutchinson@outlook.com>
2022-12-22 07:31:02 -0600

    Improve ability to maintain code

--- projects/workspace/src/app/app.module.ts
+++ projects/workspace/src/app/app.module.ts
@@ -1,29 +1,33 @@
+// angular modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgFlowchartModule } from 'projects/ng-flowchart/src/lib/ng-flowchart.module';
-import { AppComponent } from './app.component';
-import { CustomStepComponent } from './custom-step/custom-step.component';
-import { RouteStepComponent } from './custom-step/route-step/route-step.component';
-import { NestedFlowComponent } from './nested-flow/nested-flow.component';
-import { FormStepComponent } from './form-step/form-step.component';
 import { FormsModule } from '@angular/forms';
 
+// ng flowchart modules
+import { NgFlowchartModule } from 'projects/ng-flowchart/src/lib/ng-flowchart.module';
 
+// components
+import { AppComponent } from './app.component';
+import { CustomStepComponent } from './custom-step/custom-step.component';
+import { FormStepComponent } from './form-step/form-step.component';
+import { NestedFlowComponent } from './nested-flow/nested-flow.component';
+import { RouteStepComponent } from './custom-step/route-step/route-step.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CustomStepComponent,
-    RouteStepComponent,
-    NestedFlowComponent,
-    FormStepComponent
-  ],
   imports: [
     BrowserModule,
     NgFlowchartModule,
     FormsModule,
   ],
+  declarations: [
+    // components
+    AppComponent,
+    CustomStepComponent,
+    FormStepComponent,
+    NestedFlowComponent,
+    RouteStepComponent
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
