0d669c5ed4f99e5860b65a4596ba1066542c18de
Brook Hutchinson <Brookhutchinson@outlook.com>
2023-01-16 09:11:20 -0600

    Improve ability to maintain code

--- projects/ng-flowchart/src/lib/ng-flowchart.module.ts
+++ projects/ng-flowchart/src/lib/ng-flowchart.module.ts
@@ -1,15 +1,34 @@
-import { CommonModule } from '@angular/common';
+// angular modules
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// components
 import { NgFlowchartArrowComponent } from './ng-flowchart-arrow/ng-flowchart-arrow.component';
-import { NgFlowchartCanvasDirective } from './ng-flowchart-canvas.directive';
-import { NgFlowchartStepDirective } from './ng-flowchart-step.directive';
 import { NgFlowchartStepComponent } from './ng-flowchart-step/ng-flowchart-step.component';
 
+// directives
+import { NgFlowchartCanvasDirective } from './ng-flowchart-canvas.directive';
+import { NgFlowchartStepDirective } from './ng-flowchart-step.directive';
+
 @NgModule({
-    declarations: [NgFlowchartCanvasDirective, NgFlowchartStepDirective, NgFlowchartStepComponent, NgFlowchartArrowComponent],
-    imports: [
-        CommonModule
-    ],
-    exports: [NgFlowchartCanvasDirective, NgFlowchartStepDirective, NgFlowchartStepComponent, NgFlowchartArrowComponent]
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    // components
+    NgFlowchartStepComponent,
+    NgFlowchartArrowComponent,
+    // directives
+    NgFlowchartCanvasDirective,
+    NgFlowchartStepDirective
+  ],
+  exports: [
+    // components
+    NgFlowchartArrowComponent,
+    NgFlowchartStepComponent,
+    // directives
+    NgFlowchartCanvasDirective,
+    NgFlowchartStepDirective
+  ]
 })
-export class NgFlowchartModule { }
+export class NgFlowchartModule {}
