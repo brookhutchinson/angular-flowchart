6490a80622a222bbdc3cc98da307ef0491e6b78a
Brook Hutchinson <Brookhutchinson@outlook.com>
2023-01-27 08:34:19 -0600

    Improve ability to maintain code

--- projects/workspace/src/app/custom-step/route-step/route-step.component.ts
+++ projects/workspace/src/app/custom-step/route-step/route-step.component.ts
@@ -1,5 +1,10 @@
+// angular
 import { Component } from '@angular/core';
+
+// components
 import { NgFlowchartStepComponent } from 'projects/ng-flowchart/src/lib/ng-flowchart-step/ng-flowchart-step.component';
+
+// interfaces
 import { NgFlowchart } from 'projects/ng-flowchart/src';
 
 @Component({
@@ -8,18 +13,13 @@ import { NgFlowchart } from 'projects/ng-flowchart/src';
   styleUrls: ['./route-step.component.scss']
 })
 export class RouteStepComponent extends NgFlowchartStepComponent {
+  ngOnInit() {}
 
-  ngOnInit(): void {
-  }
-
-  getDropPositionsForStep(step: NgFlowchart.Step): NgFlowchart.DropPosition[] {    
-    if(step.type !== 'route-step') {
+  getDropPositionsForStep(step: NgFlowchart.Step): NgFlowchart.DropPosition[] {
+    if (step.type !== 'route-step') {
       return ['BELOW']
-    }
-    else {
+    } else {
       return ['LEFT', 'RIGHT'];
     }
-    
   }
-
 }
